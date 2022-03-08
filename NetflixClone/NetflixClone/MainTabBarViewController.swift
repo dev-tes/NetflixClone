f4ca07fdb70690689a0e74d9b63d3f03c4a8bd82
dev-tes <tesleem.amuda@gmail.com>
2022-03-09 00:33:01 +0100

    create method to configure tabs and declutter view did load

--- NetflixClone/NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/NetflixClone/MainTabBarViewController.swift
@@ -8,21 +8,33 @@
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
-
-    let viewController1 = UINavigationController(rootViewController: HomeViewController())
-    let viewController2 = UINavigationController(rootViewController: UpcomingViewController())
-    let viewController3 = UINavigationController(rootViewController: SearchViewController())
-    let viewController4 = UINavigationController(rootViewController: DownloadsViewController())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
-
+        
+        configureTabs()
+    }
+    
+    func configureTabs(){
+        
+        let viewController1 = UINavigationController(rootViewController: HomeViewController())
+        let viewController2 = UINavigationController(rootViewController: UpcomingViewController())
+        let viewController3 = UINavigationController(rootViewController: SearchViewController())
+        let viewController4 = UINavigationController(rootViewController: DownloadsViewController())
+        
+        let parentArrayOfVC: [UIViewController] = [viewController1, viewController2, viewController3, viewController4]
+        
         viewController1.tabBarItem.image = UIImage(systemName: "house")
         viewController2.tabBarItem.image = UIImage(systemName: "play.circle")
         viewController3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
         viewController4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
         
-        setViewControllers([viewController1,viewController2,viewController3,viewController4], animated: true)
+        viewController1.title = "Home"
+        viewController2.title = "Coming Soon"
+        viewController3.title = "Top Searches"
+        viewController4.title = "Downloads"
+        
+        setViewControllers(parentArrayOfVC, animated: true)
     }
 }
