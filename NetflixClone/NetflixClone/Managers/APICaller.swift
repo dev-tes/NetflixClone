7e22a6847b821bf7addfb032de647b9b863ace67
dev-tes <tesleem.amuda@gmail.com>
2022-03-12 00:43:19 +0100

    customise table view section header and create API Caller class

---
+++ NetflixClone/NetflixClone/Managers/APICaller.swift
@@ -0,0 +1,16 @@
+//
+//  APICaller.swift
+//  NetflixClone
+//
+//  Created by Tes on 12/03/2022.
+//
+
+import Foundation
+
+struct Constants{
+    static let API_Key = "ac7c844bf97f077b1e7820a068d19fd4"
+}
+
+class APICaller {
+    static let shared = APICaller()
+}
