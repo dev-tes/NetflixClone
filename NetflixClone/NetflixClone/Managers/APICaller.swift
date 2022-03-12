3319030e71fe66e493c654cb988ee3d65b31ed01
dev-tes <tesleem.amuda@gmail.com>
2022-03-12 23:33:13 +0100

    create getTrendingMovies method in APICaller class

--- NetflixClone/NetflixClone/Managers/APICaller.swift
+++ NetflixClone/NetflixClone/Managers/APICaller.swift
@@ -8,9 +8,26 @@
 import Foundation
 
 struct Constants{
-    static let API_Key = "ac7c844bf97f077b1e7820a068d19fd4"
+    static let apiKey = "ac7c844bf97f077b1e7820a068d19fd4"
+    static let baseUrl = "https://api.themoviedb.org"
 }
 
 class APICaller {
     static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping(String) -> Void){
+        guard let url = URL(string: "\(Constants.baseUrl)/3/trending/all/day?api_key=\(Constants.apiKey)") else {return}
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {return}
+            
+            do{
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
 }
