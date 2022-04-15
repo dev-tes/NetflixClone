f8ba113e878dbe20b06b4a72cd79af6c16a402a0
dev-tes <tesleem.amuda@gmail.com>
2022-04-15 22:50:27 +0100

    set and display image using SDWebImage

--- NetflixClone/NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -42,19 +42,23 @@ class CollectionViewTableViewCell: UITableViewCell {
     
     public func configure(with titles: [Title]) {
         self.titles = titles
+        DispatchQueue.main.async { [weak self] in
+            self?.collectionView.reloadData()
+        }
     }
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource  {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        titles.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else { return UICollectionViewCell()
             
         }
-        cell.configure(with: "")
+        guard let model  = titles[indexPath.row].posterPath else { return UICollectionViewCell()}
+        cell.configure(with: model )
         return cell
     }
 }
