fb095ab476aef9724d86014a76f1fecf0708c26a
dev-tes <tesleem.amuda@gmail.com>
2022-04-17 16:57:12 +0100

    create title preview view model

--- NetflixClone/NetflixClone/Controllers/General/TitlePreviewViewController.swift
+++ NetflixClone/NetflixClone/Controllers/General/TitlePreviewViewController.swift
@@ -6,11 +6,68 @@
 //
 
 import UIKit
+import WebKit
 
 class TitlePreviewViewController: UIViewController {
-
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.text = "Harry potter"
+        return label
+    }()
+    
+    private let overviewLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
+        label.text = "This is the best movie to watch as a kid"
+        return label
+    }()
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .red
+        button.setTitle("Download", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    private let webView = WKWebView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(webView)
+        view.addSubview(titleLabel)
+        view.addSubview(overviewLabel)
+        view.addSubview(downloadButton)
+        
+        configureConstraints()
     }
-
+    
+    func configureConstraints() {
+        let webviewConstraint = [
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ]
+        
+        let titleLabelConstraint = [
+            titleLabel.topAnchor.constraint(equalTo: webView.bottomAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ]
+        
+        let overviewLabelConstraint = [
+            overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor,constant: 15),
+            overviewLabel.leadingAnchor.constraint(equalTo:  view.leadingAnchor, constant: 20)
+            
+        ]
+        
+        NSLayoutConstraint.activate(webviewConstraint)
+        NSLayoutConstraint.activate(titleLabelConstraint)
+        NSLayoutConstraint.activate(overviewLabelConstraint)
+    }
+    
 }
---
+++ NetflixClone/NetflixClone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitlePreviewViewModel.swift
+//  NetflixClone
+//
+//  Created by Tes on 17/04/2022.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+}
