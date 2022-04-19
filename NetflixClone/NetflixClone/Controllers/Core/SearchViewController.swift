652052c5d03e33f35d8c622a49ce89c67af6834d
dev-tes <tesleem.amuda@gmail.com>
2022-04-19 17:08:11 +0100

    enable playing of video from search view controller

--- NetflixClone/NetflixClone/Controllers/Core/SearchViewController.swift
+++ NetflixClone/NetflixClone/Controllers/Core/SearchViewController.swift
@@ -82,6 +82,28 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         140
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        guard let titleName = title.originalTitle ?? title.originalName else { return }
+         
+        APICaller.shared.getMovie(with: titleName) { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                DispatchQueue.main.async {
+                    let vc = TitlePreviewViewController()
+                    let titlePreviewViewModel = TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverView: title.overview ?? "")
+                    vc.configure(with: titlePreviewViewModel)
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                    print(videoElement)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     
 }
 
